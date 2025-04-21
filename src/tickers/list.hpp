bfdf272fe4ece4bc9d2cd17aa3762767b2cecd4d
Martin <mwright@tonkintaylor.co.nz>
2025-04-22 11:10:42 +1200

    Creating ticker table component

--- src/tickers/list.hpp
+++ src/tickers/list.hpp
@@ -2,31 +2,55 @@
 * Copyright 2024 Martin Wright. All rights reserved.
 * Use of this source code is governed by the license found in the LICENSE file.
 */
+#pragma once
+
+#include <map>
 #include <chrono>
 #include <memory>
 #include <string>
+#include <vector>
 #include <sstream>
-#include <iomanip> 
+#include <iomanip>
 
-#include "ftxui/component/captured_mouse.hpp"
+#include "ftxui/util/ref.hpp"
+#include "ftxui/dom/table.hpp"
+#include "ftxui/dom/elements.hpp"
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/component_base.hpp"
+#include "ftxui/component/captured_mouse.hpp"
 #include "ftxui/component/screen_interactive.hpp"
-#include "ftxui/dom/elements.hpp"
-#include "ftxui/util/ref.hpp"
+
+struct SecurityData {
+    std::string ticker;
+    double price = 0.0;
+    double change = 0.0;
+    double change_percent = 0.0;
+    std::string last_update;
+};
 
 class SecurityList
 {
 // Functions
 private:
-    void _load_tickers();
+    void                                    _load_tickers();
+    void                                    _update_mock_prices(); // For mock data
+    std::vector<std::vector<std::string>>   _generate_table_data();
 public:
     SecurityList(/* args */);
     ~SecurityList();
 
+    void                update_data();
+    ftxui::Element      render_table();
+    ftxui::Component    create_component();
+
 // Attributes
 private:
-    std::string     _ticker_path = "/../assets/tickers.txt"
-    std::vector     _tickers;
+    std::string                             _ticker_path = "/../assets/tickers.txt";
+    std::vector<std::string>                _tickers;
+    std::map<std::string, SecurityData>     _securities_data;
+    std::atomic<bool>                       _is_running = true;
+    std::thread                             _update_thread;
+
 public:
+    std::atomic<bool>&                      is_running() { return _is_running; }
 };
